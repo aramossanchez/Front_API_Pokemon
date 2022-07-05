100e9e703e4ae58fe0fd5a6a503d22491c3852ad
aramossanchez <armandoramossanchez@gmail.com>
2022-07-05 10:33:12 +0200

    filtro por tipo de pokemon completo

--- pokeapp/src/app/views/pokemon-list/pokemon-list.component.ts
+++ pokeapp/src/app/views/pokemon-list/pokemon-list.component.ts
@@ -18,8 +18,10 @@ export class PokemonListComponent implements OnInit {
   
   pokemonTypes: string[] = [];
   
+  //USADO PARA SABER CUANTOS POKEMON SE HAN CARGADO EN TOTAL, Y QUITAR EL SPINNER CUANDO COINCIDA CON EL MAXIMO DE POKEMON QUE SE CARGARÁN
   numberPokemonLoaded = 0;
 
+  //NUMERO USADO PARA SABER CUANTOS POKEMON TOTALES APARECERÁN, Y QUITAR EL LOADER CUANDO COINCIDA CON EL NUMERO DE POKEMON CARGADOS
   numberPokemonListed = 0;
 
   filterOpen = false;
@@ -37,6 +39,7 @@ export class PokemonListComponent implements OnInit {
     
   }
 
+  //OBTENER LISTADO DE TODOS LOS POKEMON
   getAllPokemon() {
     this.pokemonservice.getPokemonList().subscribe(
       (res) => {
@@ -50,6 +53,7 @@ export class PokemonListComponent implements OnInit {
     )
   }
 
+  //SELECCIONAR UN POKEMON
   selectionPokemon(pokemon:PokemonListed) {
     this.pokedexStatus = true;
     this.pokemon = pokemon;
@@ -59,26 +63,28 @@ export class PokemonListComponent implements OnInit {
     this.pokedexStatus = e;
   }
 
+  //GUARDAR LOS TIPOS DE LOS POKEMON QUE SE HAN DEVUELTO DE LA API
   setPokemonTypes() {
     if(this.pokemonTypes.length === 0){
       this.pokemonTypes = this.pokemonservice.pokemonTypes;
     }
   }
 
+  //SABER CUANTOS POKEMON SE HAN CARGADO EN PANTALLA
   getNumberPokemonLoaded() {
     this.numberPokemonLoaded = this.pokemonservice.pokemonCount;
   }
 
+  //DEVUELVE CUANTOS POKEMON HAY CARGADOS EN PANTALLA
   paintNumberPokemonLoaded() {
     this.getNumberPokemonLoaded();
     return this.numberPokemonLoaded
   }
 
+  //ABRIR O CERRAR EL FILTRO
   toggleFilter() {
-
     this.filterOpen = !this.filterOpen;
     let options = document.getElementsByClassName("option");
-
     if (!this.filterOpen) {
       for (let i = 0; i < options.length; i++) {
         options[i].classList.remove("option_opened");
@@ -88,43 +94,39 @@ export class PokemonListComponent implements OnInit {
         options[i].classList.add("option_opened");
       }
     }
-
   }
 
+  //AL SELECCIONAR UN FILTRO, HACE UNA BUSQUEDA CON LOS POKEMON DE ESE TIPO
   selectFilterParameter(e:any) {
-
+    this.numberPokemonLoaded = 0;
     this.filterParameter = e.target.innerHTML;
     let filterParameter = (e.target.innerHTML).toLowerCase()
     let arrayPokemonFiltered: PokemonListed[] = [];
-
     this.pokemonservice.getPokemonByType(filterParameter).subscribe(
       (res) => {
         res.pokemon.map((pokemonObject:any) => {
-          let numberOfPokemon = pokemonObject.pokemon.url.charAt(34) + pokemonObject.pokemon.url.charAt(35) + pokemonObject.pokemon.url.charAt(36);
-          if(numberOfPokemon <= 151){
+          let numberOfPokemon = pokemonObject.pokemon.url.charAt(34) + pokemonObject.pokemon.url.charAt(35) + pokemonObject.pokemon.url.charAt(36) + pokemonObject.pokemon.url.charAt(37) + pokemonObject.pokemon.url.charAt(38);
+          let correctNumber = numberOfPokemon.split("/").join('');
+          if(correctNumber <= 151){
             arrayPokemonFiltered.push(pokemonObject.pokemon);
-            console.log(numberOfPokemon);
           }
         })
         this.pokemonList = arrayPokemonFiltered;
-        this.pokemonservice.setTotalPokemon(res.pokemon.length);
+        this.pokemonservice.setTotalPokemon(arrayPokemonFiltered.length);
+        this.numberPokemonListed = arrayPokemonFiltered.length;
       },
       (error) => {
         console.error(error);
       }
     )
-
   }
 
+  //BORRA EL FILTRO APLICADO ACTUALMENTE
   deleteFilterParameter(e:any) {
     e.stopPropagation();
+    this.numberPokemonLoaded = 0;
     this.filterParameter = null;
     this.getAllPokemon();
-  }
-
-  test(){
-    this.numberPokemonLoaded = 0;
-    this.paintNumberPokemonLoaded(); 
   }
 
 }
